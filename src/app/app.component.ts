8f7a4418e4a871bbf67aa8e781eed613d570a5f5
spencegj <spencegj@iu.edu>
2022-10-04 17:03:22 -0400

    Set up api service to handle API calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiClientService } from './services/api-client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Project8WebClient';
+
+  constructor(private apiClient: ApiClientService) {}
+
+  ngOnInit(): void {
+    // // Testing generating a random username
+    // this.apiClient.generateRandomUsername("Garrett", "Spencer").subscribe(res => {
+    //   console.log(res);
+    // });
+    // // Testing registering a new account
+    // this.apiClient.createNewAccount("Garrett", "Spencer", "test2@gmail.com", "4678392900", "testing", "admin").subscribe(res => {
+    //   console.log(res);
+    // });
+    // // Testing logging in to new account
+    // this.apiClient.loginUser("test@gmail.com", "admin").subscribe(res => {
+    //   console.log(res);
+    // });
+  }
 }
---
+++ src/app/services/api-client.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiClientService {
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  generateRandomUsername(fname_: string, lname_: string) {
+    // Generate the body object from the parameters
+    let body = {
+      fname: fname_,
+      lname: lname_
+    };
+    // Call the API, and return the observable
+    return this.httpClient.post(environment.apiUrl + "/v1/auth/generateUsername", body);
+  }
+
+  createNewAccount(fname_: string, lname_: string, email_: string, phone_: string, username_: string, password_: string) {
+    // Generate the body object from the parameters
+    let body = {
+      fname: fname_,
+      lname: lname_,
+      email: email_,
+      phone: phone_,
+      username: username_,
+      password: password_
+    };
+    // Call the API, and return the observable
+    return this.httpClient.post(environment.apiUrl + "/v1/auth/register", body);
+  }
+
+  loginUser(email_: string, password_: string) {
+    // Generate the body object from the parameters
+    let body = {
+      email: email_,
+      password: password_
+    };
+    // Call the API, and return the observable
+    return this.httpClient.post(environment.apiUrl + "/v1/auth/login", body);
+  }
+}
